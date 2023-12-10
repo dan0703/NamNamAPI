f564d4148af4c28bfda76e1a88fd3b5db2376e98
Jona <zS19023589@estudiantes.uv.mx>
2023-12-10 17:28:32 -0600

    migracion de bd

--- Models/NamnamContext.cs
+++ Models/NamnamContext.cs
@@ -35,7 +35,7 @@ public partial class NamnamContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=nam-nam-db2.mysql.database.azure.com;database=namnam;user=NamNamAdminDB;pwd=Azure123098", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+        => optionsBuilder.UseMySql("server=nam-nam-db3.mysql.database.azure.com;database=namnam;user=NamNamAdminBD;pwd=Azure123098", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
